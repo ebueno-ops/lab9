58bd73e3302203cdbff9278e902a0c51729997c9
Emmanuel Hombrebueno <emmanuel.hombrebueno@edu.sait.ca>
2022-04-10 21:27:12 -0600

    initial

---
+++ src/java/filters/AdminFilter.java
@@ -0,0 +1,66 @@
+
+package filters;
+
+import dataaccess.UserDB;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.*;
+import models.User;
+
+/**
+ *
+ * @author emman
+ */
+public class AdminFilter implements Filter {
+    /**
+     *
+     * @param request The servlet request we are processing
+     * @param response The servlet response we are creating
+     * @param chain The filter chain we are processing
+     *
+     * @exception IOException if an input/output error occurs
+     * @exception ServletException if a servlet error occurs
+     */
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        HttpSession session = httpRequest.getSession();
+
+        String email = (String) session.getAttribute("email");
+
+        // Check if the user is an admin or not.
+        UserDB userDB = new UserDB();
+        User user = userDB.get(email);
+
+        if (user.getRole().getRoleId() != 1){
+            // returns user to the notes page
+            httpResponse.sendRedirect("notes");
+            return;
+        }
+
+        chain.doFilter(request, response);
+    }
+    /**
+     * Destroy method for this filter
+     */
+    public void destroy() {        
+    }
+
+    /**
+     * Init method for this filter
+     */
+    public void init(FilterConfig filterConfig) {        
+    }
+
+    
+}
